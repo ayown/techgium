5f6b9f240a9b6eeab150d967390dd03902ed0348
KoustavBera <thekoustavofficial@gmail.com>
2025-12-03 20:10:03 +0530

    Made mongodb connection

--- backend/config/connectDB.js
+++ backend/config/connectDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import asyncHandler from "../utils/asyncHandler.js";
 
 const connectDB = asyncHandler(async () => {
-	const conn = await mongoose.connect(process.env.MONGOURI);
+	const conn = await mongoose.connect(process.env.MONGODB_URI);
 	console.log(`MongoDB connected: ${conn.connection.host}`);
 });
 
