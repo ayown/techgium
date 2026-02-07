75959704448966d5ada75947d7660754c057f732
KoustavBera <thekoustavofficial@gmail.com>
2026-02-07 19:21:32 +0530

    patch #55 - fixed some bugs and QoL improvements

--- fastapi2/app/core/reports/__init__.py
+++ fastapi2/app/core/reports/__init__.py
@@ -6,11 +6,11 @@ Two types:
 - Patient Report: Simple, color-coded, easy to understand
 - Doctor Report: Detailed biomarkers, trust envelope, technical
 """
-# Import V3 (Playwright-based) generators and export with V1-compatible names
-from .patient_reportv3 import EnhancedPatientReportGeneratorV3 as PatientReportGenerator
-from .patient_reportv3 import PatientReportV3 as PatientReport
-from .doctor_reportv3 import DoctorReportGeneratorV3 as DoctorReportGenerator
-from .doctor_reportv3 import DoctorReportV3 as DoctorReport
+# Import standard (ReportLab-based) generators
+from .patient_report import EnhancedPatientReportGenerator as PatientReportGenerator
+from .patient_report import PatientReport
+from .doctor_report import DoctorReportGenerator
+from .doctor_report import DoctorReport
 
 __all__ = [
     "PatientReportGenerator",
